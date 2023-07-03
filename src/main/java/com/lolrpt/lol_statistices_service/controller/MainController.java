49b33728e6124bcc31e52b0688ae4bf54d824886
JeonDaehong <daehong770@gmail.com>
2023-07-03 22:02:47 +0900

    2023-07-03 타임리프 Error 해결

--- src/main/java/com/lolrpt/lol_statistices_service/controller/MainController.java
+++ src/main/java/com/lolrpt/lol_statistices_service/controller/MainController.java
@@ -1,14 +1,18 @@
 package com.lolrpt.lol_statistices_service.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class MainController {
 
-    @RequestMapping("/")
+    @GetMapping("/mainPage")
     public String arriveController(){
-        return "mainPage";
+        return "index";
     }
 }
