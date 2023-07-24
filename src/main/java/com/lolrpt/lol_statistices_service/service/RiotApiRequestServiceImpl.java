f00bebe182898404098feaacdea98af4cb7539fb
JeonDaehong <daehong770@gmail.com>
2023-07-24 22:26:04 +0900

    2023-07-24 챔피언 숙련도 Insert 관련 JPA, Logic 개발

--- src/main/java/com/lolrpt/lol_statistices_service/service/RiotApiRequestServiceImpl.java
+++ src/main/java/com/lolrpt/lol_statistices_service/service/RiotApiRequestServiceImpl.java
@@ -132,6 +132,7 @@ public class RiotApiRequestServiceImpl implements RiotApiRequestService {
                     }
                     number ++;
                 }
+
                 loLUserRepository.saveAll(loLUserMasterList); // JPA Save ( saveAll(); )
             }
 
