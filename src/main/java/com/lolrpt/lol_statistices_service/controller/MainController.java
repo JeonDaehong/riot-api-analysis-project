c0fedf017f566bae6c417c4a22fb0acdac54d6f9
JeonDaehong <daehong770@gmail.com>
2023-08-12 22:38:04 +0900

    2023-08-12

--- src/main/java/com/lolrpt/lol_statistices_service/controller/MainController.java
+++ src/main/java/com/lolrpt/lol_statistices_service/controller/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/mainPage")
-    public String arriveController(){
+    public String mainController(){
         return "mainPage";
     }
 }
